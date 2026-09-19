fc0a310f01a64f771f75cccc075740886959c333
Zander Goodwin <zanderngoodwin@gmail.com>
2026-09-19 11:20:05 -0300

    Added more GUI buttons for extra variables

--- Assets/Scripts/AdjustScript.cs
+++ Assets/Scripts/AdjustScript.cs
@@ -15,17 +15,33 @@ public class AdjustScript : MonoBehaviour
         }
         if (GUI.Button(new Rect(10, 340, 100, 30), "Experience Up"))
         {
-            GameManager.instance.experiencePoints++;
+            GameManager.instance.experiencePoints += 5;
         }
         if (GUI.Button(new Rect(10, 380, 100, 30), "Experience Down"))
         {
-            GameManager.instance.experiencePoints--;
+            GameManager.instance.experiencePoints -= 5;
         }
-        if (GUI.Button(new Rect(10, 420, 100, 30), "Save"))
+        if (GUI.Button(new Rect(10, 420, 100, 30), "Score Up"))
+        {
+            GameManager.instance.score += 10;
+        }
+        if (GUI.Button(new Rect(10, 460, 100, 30), "Score Down"))
+        {
+            GameManager.instance.score -= 10;
+        }
+        if (GUI.Button(new Rect(10, 500, 100, 30), "Charge Mana"))
+        {
+            GameManager.instance.mana += 3;
+        }
+        if (GUI.Button(new Rect(10, 540, 100, 30), "Cast Spell"))
+        {
+            GameManager.instance.mana -= 3;
+        }
+        if (GUI.Button(new Rect(10, 580, 100, 30), "Save"))
         {
             GameManager.instance.Save();
         }
-        if (GUI.Button(new Rect(10, 460, 100, 30), "Load"))
+        if (GUI.Button(new Rect(10, 620, 100, 30), "Load"))
         {
             GameManager.instance.Load();
         }
